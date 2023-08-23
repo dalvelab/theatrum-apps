48d7ac44f5ddcd9cf81dc39d7c7b4926778a9d70
Евгений Брагин <dalvelab@gmail.com>
2023-08-23 18:14:13 +0500

    add small ui changes

--- src/entities/event/ui/Roles.tsx
+++ src/entities/event/ui/Roles.tsx
@@ -14,12 +14,21 @@ export const Roles: React.FC<RoleProps> = ({ data }) => {
   return (
     <Flex flexDir="column" gap={5} alignItems="flex-start">
       <Flex mt={7} flexWrap="wrap" gap={5}>
-        {roles.map(({ id, role, name }) => (
-          <Flex w="228px" key={id} flexDir="column" gap={2}>
-            <Text color="brand.300" fontSize="md" lineHeight={1}>{role}</Text>
-            <Text color="gray.900" fontSize="lg" lineHeight={1}>{name}</Text>
-          </Flex>
-        ))}
+        {roles.map(({ id, role, name }) => {
+          const names = name.split(',');
+
+          return (
+            <Flex w="228px" key={id} flexDir="column" gap={2}>
+              <Text color="brand.300" fontSize="md" lineHeight={1}>{role}</Text>
+              {names.length > 1 ? (
+                <Flex flexDir="column" gap={2}>
+                {names.map((name, index) => <Text key={index} color="gray.900" fontSize="lg" lineHeight={1}>{name}</Text>)}
+              </Flex>
+              ) : <Text color="gray.900" fontSize="lg" lineHeight={1}>{name}</Text>}
+            </Flex>
+          )
+        }
+        )}
       </Flex>
       {data.length > 5 && (
         <Button 
